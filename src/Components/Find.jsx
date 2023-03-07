a22d0ad749685ef269cca28d03ed00ac5be51ae1
Vilas Chavhan <chavhanvilas02@gmail.com>
2023-03-07 16:33:47 +0530

    search page

--- src/Components/Find.jsx
+++ src/Components/Find.jsx
@@ -1,8 +1,13 @@
+import { BsSearch } from 'react-icons/bs'
 export default function Find() {
     return (
         <div className="bg-black md:min-h-[90.5vh] px-5">
-            <div>
-                
+            <div className="text-white pt-5 flex justify-between items-center">
+                <h1 className="text-3xl font-serif font-semibold">Search Now</h1>
+                <div className="flex items-center bg-white text-black caret-red-500 hover:outline hover:outline-2 hover:outline-red-500 px-4 py-1 rounded-md">
+                    <BsSearch className='text-xl'/>
+                    <input type="text" className="pl-2 outline-none"/>
+                </div>
             </div>
         </div>
     );
