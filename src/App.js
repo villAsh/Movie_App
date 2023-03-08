8b1a808ac2598e561bc7196809891b45e08293d4
Vilas Chavhan <chavhanvilas02@gmail.com>
2023-03-08 11:05:34 +0530

    added lazy loading to optimize load time

--- src/App.js
+++ src/App.js
@@ -1,31 +1,71 @@
-import Navbar from "./Components/Navbar";
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './App/store';
+import Navbar from './Components/Navbar';
 
-import { Routes,Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./App/store";
-import SingleContent from "./Components/SingleContent";
-import WatchList from "./Components/WatchList";
-import Movies from "./Components/Movies";
-import Shows from "./Components/Shows";
-import Find from "./Components/Find";
-
+const SingleContent = lazy(() => import('./Components/SingleContent'));
+const WatchList = lazy(() => import('./Components/WatchList'));
+const Movies = lazy(() => import('./Components/Movies'));
+const Shows = lazy(() => import('./Components/Shows'));
+const Find = lazy(() => import('./Components/Find'));
 
 function App() {
-  return (
-    <Provider store={store} >
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<Shows />} />
-          <Route path="details/:id" element={<SingleContent type="tv"/>} />
-          <Route path="movies/details/:id" element={<SingleContent type="movie"/>} />
-          <Route path="watchlist" element={<WatchList />}/>
-          <Route path="movies" element={<Movies />} />
-          <Route path="find" element={ <Find /> } />
-        </Routes>
-  
-    </Provider>
-
-  );
+	return (
+		<Provider store={store}>
+			<Navbar />
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<Suspense fallback={<h1>Loading....</h1>}>
+							<Shows />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="details/:id"
+					element={
+						<Suspense  fallback={<h1>Loading....</h1>}>
+							<SingleContent type="tv" />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="movies/details/:id"
+					element={
+						<Suspense  fallback={<h1>Loading....</h1>}>
+							<SingleContent type="movie" />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="watchlist"
+					element={
+						<Suspense  fallback={<h1>Loading....</h1>}>
+							<WatchList />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="movies"
+					element={
+						<Suspense  fallback={<h1>Loading....</h1>}>
+							<Movies />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="find"
+					element={
+						<Suspense  fallback={<h1>Loading....</h1>}>
+							<Find />
+						</Suspense>
+					}
+				/>
+			</Routes>
+		</Provider>
+	);
 }
 
 export default App;
--- src/Components/Find.jsx
+++ src/Components/Find.jsx
@@ -4,7 +4,7 @@ export default function Find() {
         <div className="bg-black md:min-h-[90.5vh] px-5">
             <div className="text-white pt-5 flex justify-between items-center">
                 <h1 className="text-3xl font-serif font-semibold">Search Now</h1>
-                <div className="flex items-center bg-white text-black caret-red-500 hover:outline hover:outline-2 hover:outline-red-500 px-4 py-1 rounded-md">
+                <div className="md:mt-3 flex items-center justify-between bg-white text-black caret-red-500 hover:outline hover:outline-2 hover:outline-red-500 px-4 py-1 rounded-md">
                     <BsSearch className='text-xl'/>
                     <input type="text" className="pl-2 outline-none"/>
                 </div>
