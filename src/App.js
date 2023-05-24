26880ecad5ff46d0b14e85900ec4e65d196e30f7
Vilas Chavhan <50195101+villAsh@users.noreply.github.com>
2023-05-24 13:08:38 +0530

    updated lazy loading
    added fallback Loading text

--- src/App.js
+++ src/App.js
@@ -1,103 +1,113 @@
-import { lazy, Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './App/store';
-import Navbar from './Components/Navbar';
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./App/store";
+import Navbar from "./Components/Navbar";
 
-const SingleContent = lazy(() => import('./Components/SingleContent'));
-const WatchList = lazy(() => import('./Components/WatchList'));
-const Movies = lazy(() => import('./Components/Movies'));
-const Shows = lazy(() => import('./Components/Shows'));
-const Find = lazy(() => import('./Components/Find'));
+const SingleContent = lazy(() => import("./Components/SingleContent"));
+const WatchList = lazy(() => import("./Components/WatchList"));
+const Movies = lazy(() => import("./Components/Movies"));
+const Shows = lazy(() => import("./Components/Shows"));
+const Find = lazy(() => import("./Components/Find"));
+
+const Loading = () => {
+  return (
+    <div className="h-[80vh] w-screen flex items-center justify-center bg-black text-2xl">
+      <div>
+        <h1 className="text-5xl text-red font-bold">Loading....</h1>
+      </div>
+    </div>
+  );
+};
 
 function App() {
-	return (
-		<Provider store={store}>
-			<Navbar />
-			<Routes>
-				<Route
-					path="/"
-					element={
-						<Suspense fallback={<h1>Loading....</h1>}>
-							<Shows />
-						</Suspense>
-					}
-				/>
-				<Route
-					path="show/:id"
-					element={
-						<Suspense fallback={<h1>Loading....</h1>}>
-							<SingleContent type="tv" />
-						</Suspense>
-					}
-				/>
-				<Route
-					path="movies/movie/:id"
-					element={
-						<Suspense fallback={<h1>Loading....</h1>}>
-							<SingleContent type="movie" />
-						</Suspense>
-					}
-				/>
-				
-				<Route
-					path="watchlist/tv/:id"
-					element={
-						<Suspense fallback={<h1>Loading....</h1>}>
-							<SingleContent type="tv" />
-						</Suspense>
-					}
-				/>	<Route
-				path="watchlist/movie/:id"
-				element={
-					<Suspense fallback={<h1>Loading....</h1>}>
-						<SingleContent type="movie" />
-					</Suspense>
-				}
-			/>
-				<Route
-					path={`find/tv/:id`}
-					element={
-						<Suspense fallback={<h1>Loading....</h1>}>
-							<SingleContent type="tv" />
-						</Suspense>
-					}
-				/>
-				<Route
-					path={`find/movie/:id`}
-					element={
-						<Suspense fallback={<h1>Loading....</h1>}>
-							<SingleContent type="movie" />
-						</Suspense>
-					}
-				/>
-				<Route
-					path="watchlist"
-					element={
-						<Suspense fallback={<h1>Loading....</h1>}>
-							<WatchList />
-						</Suspense>
-					}
-				/>
-				<Route
-					path="movies"
-					element={
-						<Suspense fallback={<h1>Loading....</h1>}>
-							<Movies />
-						</Suspense>
-					}
-				/>
-				<Route
-					path="find"
-					element={
-						<Suspense fallback={<h1>Loading....</h1>}>
-							<Find />
-						</Suspense>
-					}
-				/>
-			</Routes>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Shows />
+            </Suspense>
+          }
+        />
+        <Route
+          path="show/:id"
+          element={
+            <Suspense fallback={<Loading />}>
+              <SingleContent type="tv" />
+            </Suspense>
+          }
+        />
+        <Route
+          path="movies/movie/:id"
+          element={
+            <Suspense fallback={<Loading />}>
+              <SingleContent type="movie" />
+            </Suspense>
+          }
+        />
+        <Route
+          path="watchlist/tv/:id"
+          element={
+            <Suspense fallback={<Loading />}>
+              <SingleContent type="tv" />
+            </Suspense>
+          }
+        />{" "}
+        <Route
+          path="watchlist/movie/:id"
+          element={
+            <Suspense fallback={<Loading />}>
+              <SingleContent type="movie" />
+            </Suspense>
+          }
+        />
+        <Route
+          path={`find/tv/:id`}
+          element={
+            <Suspense fallback={<Loading />}>
+              <SingleContent type="tv" />
+            </Suspense>
+          }
+        />
+        <Route
+          path={`find/movie/:id`}
+          element={
+            <Suspense fallback={<Loading />}>
+              <SingleContent type="movie" />
+            </Suspense>
+          }
+        />
+        <Route
+          path="watchlist"
+          element={
+            <Suspense fallback={<Loading />}>
+              <WatchList />
+            </Suspense>
+          }
+        />
+        <Route
+          path="movies"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Movies />
+            </Suspense>
+          }
+        />
+        <Route
+          path="find"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Find />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </Provider>
+  );
 }
 
 export default App;
